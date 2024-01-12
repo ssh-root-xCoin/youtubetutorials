d2d57d075223f6ba2541e58ab6f0ab168a309116
xCoincidence <mreyansh614@gmail.com>
2024-01-12 19:56:54 +0530

    changed spelling mistake

--- Password Manager.py
+++ Password Manager.py
@@ -1,7 +1,7 @@
 passworDict = {
     'facebook':'Username: Sigma69 Password: Cornoncob420',
     'twitter':"Username: @Sigma69_1337 Password: Lol",
-    "lacob d'souza" : "Username: ilostmybookbloxchannel Password: LolIactuallylostmyaccountlikeimagine",
+    "jacob d'souza" : "Username: ilostmybookbloxchannel Password: LolIactuallylostmyaccountlikeimagine",
     "lola" : "Username: @xCoin_ Password: TGJSAPO12412515"
 }
 passwordrec = input('Enter the social media site or app you want login for Eg. youtube, facebook, twitter\n')
